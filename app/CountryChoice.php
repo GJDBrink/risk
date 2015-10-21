4ae968436afc12f8331dfaac637bc6548e2992d3
GJDBrink <gjdbrink@gmail.com>
2015-10-21 20:04:13 +0200

    choose and player add

---
+++ app/CountryChoice.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CountryChoice extends Model
+{
+    //
+}
--- resources/views/game/start.blade.php
+++ resources/views/game/start.blade.php
@@ -7,18 +7,137 @@
  */
 
         ?>
+<html>
+    <head>
+        <title>Risk Game</title>
 
-<style>
-    .alaska {
-        width: 200px;
-        height: 180px;
-        background-color: lightyellow;
-    }
-</style>
-
-Start the game
+        <link href="{{ asset('/assets/css/bootstrap.min.css') }}" rel="stylesheet" />
+        <link href="{{ asset('/assets/css/territories.css') }}" rel="stylesheet" />
 
 
-<div class="alaska">
-    Alaska
-</div>
+    </head>
+
+    <body>
+
+
+
+
+    <div class="container">
+
+        <div class="row">
+
+            <button type="button" class="btn btn-info">Start the game</button>
+
+        </div>
+
+        <div style="clear: both;"></div>
+
+        <div class="west">
+
+            <div class="north-america">
+
+                <div class="alaska">
+                    Alaska
+                </div>
+
+                <div class="north-west">
+                    North-West Territories
+                </div>
+
+                <div class="greenland">
+                    Greenland
+                </div>
+
+                <div class="alberta">
+                    Alberta
+                </div>
+
+                <div class="ontario">
+                    Ontario
+                </div>
+
+                <div class="quebec">
+                    Quebec
+                </div>
+
+                <div class="western-us">
+                    Western United States
+                </div>
+
+                <div class="eastern-us">
+                    Eastern United Staetes
+                </div>
+
+                <div class="clear"></div>
+
+                <div class="central-america">
+                    Central America
+                </div>
+
+            </div>
+
+            <div class="clear"></div>
+
+            <div class="south-america">
+
+                <div class="venezuela">
+                    Venezuela
+                </div>
+
+                <div class="peru">
+                    Peru
+                </div>
+
+                <div class="brazil">
+                    Brazil
+                </div>
+
+                <div class="argentina">
+                    Argentina
+                </div>
+
+            </div>
+        </div>
+
+        <div class="middle">
+
+            <div class="europe">
+                <div class="iceland">
+                    Iceland
+                </div>
+
+                <div class="scandinavia">
+                    Scandinavia
+                </div>
+
+                <div class="great-britain">
+                    Great Britain
+                </div>
+
+                <div class="north-europe">
+                    North Europe
+                </div>
+
+                <div class="east-europe">
+                    Eastern Euope
+                </div>
+
+                <div class="west-europe">
+                    West Europe
+                </div>
+
+                <div class="south-europe">
+                    South Europe
+                </div>
+
+
+
+            </div>
+
+        </div>
+
+    </div>
+
+    </body>
+
+</html>
